b3accf21775c31d74cdfe2e87739bc349fd35aef
alchemyunited <mark.simchock@alchemyunited.com>
2014-09-12 17:04:02 -0400

    made xml more xpath() friendly

--- class-wp-ezclasses-standard-email-to-sms.php
+++ class-wp-ezclasses-standard-email-to-sms.php
@@ -1,8 +1,25 @@
 <?php
 
-/**
- * TODO - Methods to get the XML
- */
+if ( ! class_exists('Class_WP_ezClasses_Standard_Email_To_SMS') ){
+  class Class_WP_ezClasses_Standard_Email_To_SMS{
+  
+    function __construct(){
+  
+      $url = dirname(__FILE__) . '/xml/standard-email-to-sms.xml';
+	
+	  $xml_carriers = simplexml_load_file($url);
+	
+	  // xpath trial 1
+	  $result = $xml_carriers->xpath("international/country[@abbr2='ca' and @abbr3='can']");
+	  var_dump( $result );
+	
+	  //xpath trial 2
+	  $result = $xml_carriers->xpath("international/country/carriers/carrier[@option='acs_wireless_us' and @active=true]");
+	  var_dump( $result );
+ 
+    }
 
+  }
+}
 
 ?>
